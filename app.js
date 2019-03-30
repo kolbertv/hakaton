2e3a6e31898dc868f2c0a2d581102d884fc3f0aa
kolbert <kolbert@yandex.ru>
2019-03-31 02:36:50 +0300

    sdf

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ const iotRoutes = require('./routes/iot');
 const app = express();
 
 app.use((req, res, next) => {
-     res.setHeader('Access-Control-Allow-Origin', '*');
-     res.setHeader(
-         'Access-Control-Allow-Methods',
-         'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-     );
-     res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
      next();
  });
 
