e2e28864e8c8383926b86a431ff546e2a04431d7
kolbert <kolbert@yandex.ru>
2019-03-31 02:24:10 +0300

    cors

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const mongoose = require('mongoose');
 const iotRoutes = require('./routes/iot');
 
 
-
 const app = express();
+
+app.use((req, res, next) => {
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader(
+         'Access-Control-Allow-Methods',
+         'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+     );
+     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+     next();
+ });
 
 app.use(bodyParser.urlencoded({
     extended: false
